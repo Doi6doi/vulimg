886965fedebbb6c945e6c9b6d4d4272d3c4c756b
Várnagy Zoltán <doi6doi@gmail.com>
2024-08-31 20:59:35 -0600

    fejl

--- vulimg.h
+++ vulimg.h
@@ -25,6 +25,7 @@ typedef struct VigImage * VigImage;
 int vig_error();
 /// check result and fail if not ok
 void vig_check_fail();
+
 /// initialize image operations
 bool vig_init( VcpVulcomp v );
 /// terminate image operations
@@ -41,22 +42,21 @@ VigPixel vig_image_pixel( VigImage );
 VigCoord vig_image_width( VigImage );
 /// get image height
 VigCoord vig_image_height( VigImage );
-
 /// is image full or a part
 bool vig_image_full( VigImage );
 /// get image data address
 void * vig_image_address( VigImage );
-/// get image start in bits relative to address
-uint32_t vig_image_start( VigImage );
 /// get image stride in dwords
 uint32_t vig_image_stride( VigImage );
 /// get image storage
-VcpStorage vig_image_storage( VigImage ); 
+VcpStorage vig_image_storage( VigImage );
 /// destroy image
 void vig_image_free( VigImage );
+
 /// part of an image
 VigImage vig_image_part( VigImage img, VigCoord left, VigCoord top,
    VigCoord width, VigCoord height );
+
 /// copy image
 bool vig_image_copy( VigImage src, VigImage dst );
 /// add a plane to image
@@ -65,18 +65,16 @@ bool vig_image_join( VigImage dst, VigImage src, VigPlane plane );
 bool vig_image_plane( VigImage src, VigPlane plane, VigImage dst );
 /// scale image
 bool vig_image_scale( VigImage src, VigImage dst );
-/* get average pixel value of image
-bool vig_image_avg( VigImage img, uint32_t * avg );
-/// pixel value differences between images
-bool vig_image_diff( VigImage a, VigImage b, uint32_t * diff );
-*/
-/// reads bmp
-VigImage vig_bmp_read( void * stream, VtlStreamOp read );
-/// write bmp
-bool vig_bmp_write( VigImage src, void * stream, VtlStreamOp write );
+
 /// reads raw image
 bool vig_raw_read( VigImage img, void * stream, VtlStreamOp read, bool pad );
 /// writes raw image
 bool vig_raw_write( VigImage img, void * stream, VtlStreamOp write, bool pad );
+
+/// reads bmp
+VigImage vig_bmp_read( void * stream, VtlStreamOp read );
+/// write bmp
+bool vig_bmp_write( VigImage src, void * stream, VtlStreamOp write );
+
 
 #endif // VULIMGH
